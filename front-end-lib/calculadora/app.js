3ac3f83f05f9097b5aa06bf8863c00a5eaa58046
rdmd <ricardo.d.martin@gmail.com>
2022-03-01 17:57:50 -0300

    Agrego boton de clear y display

--- front-end-lib/calculadora/app.js
+++ front-end-lib/calculadora/app.js
@@ -8,12 +8,13 @@ class App extends React.Component{
         let calcNumbers=["zero","one","two","three","four","five","six","seven","eight","nine"].map(
         (x,idx)=>button({text:idx,id:x}))
         let calcOperations=[{symbol:'+',id:"add"},{symbol:'-',id:"subtract"},{symbol:'*',id:"multiply"},{symbol:'/',id:"divide"},{symbol:".",id:"decimal"}].map((el)=>button({text:el.symbol,id:el.id}));
-
-        return React.createElement("div",{id:"calculadora"},["hola",calcButtons,calcNumbers,calcOperations])
+        let clear=button({text:"clear",id:"clear"})
+        let calcDisplay=display({})
+        return React.createElement("div",{id:"calculadora"},["hola",calcButtons,calcNumbers,calcOperations,clear,calcDisplay])
     }
 }
 const button=(props)=> React.createElement("button",{id:props.id},[props.text])
-
+const display=(props)=>React.createElement("input",{readOnly:"readonly",id:"display"})
 const domContainer=document.querySelector("#app");
 ReactDOM.render(React.createElement(App,{}),domContainer);
 
