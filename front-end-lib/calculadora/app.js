5b1bd5f8bb873e752a40c666921deb14a524fcfc
rdmd <ricardo.d.martin@gmail.com>
2022-03-02 10:15:06 -0300

    Termine el ingreso de numeros.

--- front-end-lib/calculadora/app.js
+++ front-end-lib/calculadora/app.js
@@ -1,20 +1,75 @@
+const checkZeros=/^[^0.]*[1-9]+\d*\.?\d*$/
+const startsWithZeroes=/^[0]+.*$/
+checkOperation=/^[+\-*/]{1}-?$/
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={
+            formula:'',
+            lastNumber:'0',
+            lastOperation:'',
+        }
+        this.inputNumber=this.inputNumber.bind(this)
+        this.inputOperation=this.inputOperation.bind(this)
+        this.clear=this.clear.bind(this)
+        this.equals=this.equals.bind(this)
+    }
+    inputNumber(event){
+        this.setState((oldState)=>{
+            let lastNumber=startsWithZeroes.test(oldState.lastNumber)?
+                event.target.innerText:oldState.lastNumber+event.target.value;
+            lastNumber=checkZeros.test(lastNumber)?lastNumber:oldState.lastNumber;
+            return{
+                formula:oldState.formula+oldState.lastOperation,
+                lastNumber,
+                lastOperation:'',
+            }
+        })
+    }
+    inputOperation(event){
+        this.setState((oldState)=>{
+            let formula=oldState.formula+oldState.lastNumber;
+            return{
+                formula,
+                lastNumber:'',
+                lastOperation:event.target.innerText,
+            }
+        })
+    }
+    clear(){
+        console.log(this.state)
+        this.setState({formula:'',
+            lastOperation:'',
+            lastNumber:'0',
+        })
+    }
+    equals(){
+    let formula=this.state.formula==='0'?this.state.lastOperation+this.state.lastNumber:this.state.formula+this.state.lastOperation+this.state.lastNumber;
+
+    this.setState({
+        formula:`${eval(formula)}`,
+        lastOperation:'',
+        lastNumber:'',
+    })
     }
     render(){
-        let calcButtons=button({text:"=",id:"equals"})
+        let calcButtons=button({text:"=",id:"equals",click:this.equals})
+        
         let calcNumbers=["zero","one","two","three","four","five","six","seven","eight","nine"].map(
-        (x,idx)=>button({text:idx,id:x}))
-        let calcOperations=[{symbol:'+',id:"add"},{symbol:'-',id:"subtract"},{symbol:'*',id:"multiply"},{symbol:'/',id:"divide"},{symbol:".",id:"decimal"}].map((el)=>button({text:el.symbol,id:el.id}));
-        let clear=button({text:"clear",id:"clear"})
-        let calcDisplay=display({})
+        (x,idx)=>button({text:idx,id:x,click:this.inputNumber}))
+
+        calcNumbers.push(button({text:".",id:"decimal",click:this.inputNumber}))
+
+        let calcOperations=[{symbol:'+',id:"add"},{symbol:'-',id:"subtract"},{symbol:'*',id:"multiply"},{symbol:'/',id:"divide"},].map((el)=>button({text:el.symbol,id:el.id,click:this.inputOperation}));
+
+        let clear=button({text:"clear",id:"clear",clear:this.clear,click:this.clear})
+        let calcDisplay=display({value:this.state.formula+this.state.lastNumber+this.state.lastOperation})
+
         return React.createElement("div",{id:"calculadora"},["hola",calcButtons,calcNumbers,calcOperations,clear,calcDisplay])
     }
 }
-const button=(props)=> React.createElement("button",{id:props.id},[props.text])
-const display=(props)=>React.createElement("input",{readOnly:"readonly",id:"display"})
+const button=(props)=> React.createElement("button",{id:props.id,value:props.text,onClick:props.click},[props.text])
+const display=(props)=>React.createElement("input",{readOnly:"readonly",id:"display",value:props.value})
 const domContainer=document.querySelector("#app");
 ReactDOM.render(React.createElement(App,{}),domContainer);
 
