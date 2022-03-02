278a2ed37a05d8e88130d359b555be1291d7ec3d
rdmd <ricardo.d.martin@gmail.com>
2022-03-02 12:57:51 -0300

    Termino parte visual del reloj

--- front-end-lib/reloj25y5/app.js
+++ front-end-lib/reloj25y5/app.js
@@ -1,21 +1,40 @@
+const defaultState={
+    breakLength:5,
+    sessionLength:25,
+}
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={...defaultState}
+        this.reset=this.reset.bind(this);
+    }
+    reset(){
+
     }
     render(){
         let breakLabel=lengthComponent({id:"break-label",text:"break"})
-        let sessionLabel=lengthComponent({id:"session-label",text:"session"})
+        let breakLength=React.createElement("h2",{id:"break-length"},this.state.breakLength)
         let breakDec=clickeables({id:"break-decrement",text:"break dec time"})
-        let sessionDec=clickeables({id:"session-decrement",text:"session dec time"})
         let breakInc=clickeables({id:"break-increment",text:"break increment"})
-        let sessionInc=clickeables({id:"session-increment",text:"session increment"});
+        let breakPanel=panel({className:"breakPanel",id:"break-panel"},[breakLabel,breakLength,breakDec,breakInc]) 
 
-        return React.createElement("div",{},[breakLabel,sessionLabel,breakDec,sessionDec,breakInc,sessionInc])
+        let sessionInc=clickeables({id:"session-increment",text:"session increment"});
+        let sessionLength=React.createElement("h2",{ id:"session-length"},this.state.sessionLength)
+        let sessionLabel=lengthComponent({id:"session-label",text:"session"})
+        let sessionDec=clickeables({id:"session-decrement",text:"session dec time"})
+        let sessionPanel=panel({className:"sessionPanel",id:"session-panel"},[sessionLabel,sessionLength,sessionInc,sessionDec]);
+
+        let timer=React.createElement("div",{ id:"timer-label",},["session"])
+        let timeLeft=React.createElement("div",{id:"time-left"},["25:00"]);
+        let start=clickeables({id:"start_stop"})
+        let reset=clickeables({id:"reset"}) 
+
+        return React.createElement("div",{id:"timer-root"},[breakPanel,sessionPanel,timer,timeLeft,start,reset])
     }
 }
 const lengthComponent=(props)=> React.createElement("div",{id:props.id},[props.text])
 const clickeables=(props)=>React.createElement("button",{id:props.id},[props.text])
+const panel=(props,childs)=>React.createElement("span",{className:props.className,id:props.id},childs)
 const domContainer=document.querySelector("#app");
 ReactDOM.render(React.createElement(App,{}),domContainer);
 
